8f150b7b6caa20559003843792d41fa5adfcb1e5
tashaband <tanvi.s.lotlikar@gmail.com>
2014-02-26 21:56:40 -0800

    fix

--- ryu/app/simple_switchParser1.py
+++ ryu/app/simple_switchParser1.py
@@ -22,10 +22,10 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_0
-from ryu.lib.mac import haddr_to_bin
+from ryu.lib.mac import haddr_to_str
 from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
-from array import *
+
+
 
 
 class SimpleSwitch(app_manager.RyuApp):
@@ -76,40 +76,36 @@ class SimpleSwitch(app_manager.RyuApp):
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
-
+        self.packetParser(msg)
+        dst, src, _eth_type = struct.unpack_from('!6s6sH', buffer(msg.data), 0)
         
-
-        pkt = packet.Packet(msg.data)
-        self.packetParser(pkt)
-        eth = pkt.get_protocol(ethernet.ethernet)
-
-        dst = eth.dst
-        src = eth.src
-
         dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
-
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, msg.in_port)
-
+        
+        self.logger.debug("packet in %s %s %s %s",
+                          dpid, haddr_to_str(src), haddr_to_str(dst),
+                          msg.in_port)
+        
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = msg.in_port
-
+        
         if dst in self.mac_to_port[dpid]:
             out_port = self.mac_to_port[dpid][dst]
         else:
             out_port = ofproto.OFPP_FLOOD
-
+        
         actions = [datapath.ofproto_parser.OFPActionOutput(out_port), 
                    datapath.ofproto_parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
-
+        
         # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
             self.add_flow(datapath, msg.in_port, dst, actions)
-
+        
         out = datapath.ofproto_parser.OFPPacketOut(
             datapath=datapath, buffer_id=msg.buffer_id, in_port=msg.in_port,
             actions=actions)
         datapath.send_msg(out)
+
 
     @set_ev_cls(ofp_event.EventOFPPortStatus, MAIN_DISPATCHER)
     def _port_status_handler(self, ev):
