c70c10ca19cd5be173097d085ab4e2678be0ddbf
tashaband <tanvi.s.lotlikar@gmail.com>
2014-03-25 15:19:28 -0700

    rules framework -1 <Tanvi>

--- ryu/lib/ids/ids_monitor.py
+++ ryu/lib/ids/ids_monitor.py
@@ -1,10 +1,5 @@
-
-
-
 import os
 import gevent
-
-
 from ryu.base import app_manager
 from ryu.controller import event
 from array import *
@@ -17,15 +12,19 @@ class AttackAlert(event.EventBase):
         super(AttackAlert, self).__init__()
         self.alertmsg = alertmsg
         self.data = data
-
+        
 
 class IDSMonitor(app_manager.RyuApp):
     def __init__(self):
         super(IDSMonitor, self).__init__()
         self.name = 'ids_monitor'
+        self.rules = []
+        gevent.spawn_later(0, self.read_rules())
 
 
     def check_packet(self,msg):
+        for rule in rules:
+            print rule
         my_array = array('B', msg.data)
         pkt = packet.Packet(my_array)
         for p in pkt:
@@ -33,3 +32,10 @@ class IDSMonitor(app_manager.RyuApp):
             if p.protocol_name == 'icmp':
                 alertmsg = 'ICMP ATTACK'
                 self.send_event_to_observers(AttackAlert(alertmsg,msg))
+
+    def read_rules(self):
+        print 'Reading SNORT RULES file'
+        fname = 'rules.txt'
+        with open(fname) as f:
+            self.rules = f.readlines()
+            
