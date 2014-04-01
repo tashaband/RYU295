0a7bea040541bbc5a51ce3d8ae55e4ad6b466121
tashaband <tanvi.s.lotlikar@gmail.com>
2014-04-01 10:22:42 -0700

    read rule and call protocol file <Tanvi>

--- ryu/lib/ids/tcp.py
+++ ryu/lib/ids/tcp.py
@@ -10,18 +10,21 @@ class tcp(object):
 
     def check_packet(self,mode,src_ip, src_port, dst_ip, dst_port): 
         pkt = packet.Packet(self.packet_data.data)
-        ip_pkt = pkt.get_protocol(ipv4.ipv4)
+        for p in pkt:
+            print p.protocol_name
+            if p.protocol_name == 'tcp':
+                ip_pkt = pkt.get_protocol(ipv4.ipv4)
         
-        p_dst = ip_pkt.dst
+                p_dst = ip_pkt.dst
 
-        p_src = ip_pkt.src
-        print 'packet source', p_src
-        print 'packet dst', p_dst
-        print 'rule source', src_ip
-        print 'rule dst', dst_ip
-        if ((src_ip == 'any') or (src_ip == p_src)):
-            if ((dst_ip == 'any') or (dst_ip == p_dst)):
-                if mode == 'alert':
-                    print 'Ethernet Attack Packet'
-                    alertmsg = 'Ethernet Attack Packet'
-                    self.send_event_to_observers(AttackAlert(alertmsg,msg))
+                p_src = ip_pkt.src
+                print 'packet source', p_src
+                print 'packet dst', p_dst
+                print 'rule source', src_ip
+                print 'rule dst', dst_ip
+                if ((src_ip == 'any') or (src_ip == p_src)):
+                    if ((dst_ip == 'any') or (dst_ip == p_dst)):
+                        if mode == 'alert':
+                            print 'TCP Attack Packet'
+                            alertmsg = 'TCP Attack Packet'
+                            self.send_event_to_observers(AttackAlert(alertmsg,msg))
