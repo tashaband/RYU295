25f015a67a196c28119bf1f3122a3afe268351db
tashaband <tanvi.s.lotlikar@gmail.com>
2014-04-09 16:12:17 -0700

    trying to print payload as string <Tanvi>

--- ryu/lib/ids/ids_utils.py
+++ ryu/lib/ids/ids_utils.py
@@ -4,41 +4,46 @@ UNPACK_STR = '!%ds'
 
 def get_packet_dst_ip_address(pkt):
     for p in pkt:
-            if p.protocol_name == 'ipv4':
-                p_dst = p.dst
-                return p_dst
+            if hasattr(p, 'protocol_name') is True:
+                if p.protocol_name == 'ipv4':
+                    p_dst = p.dst
+                    return p_dst
     return None
 
 
 
 def get_packet_src_ip_address(pkt):
     for p in pkt:
-            if p.protocol_name == 'ipv4':
-                p_src = p.src
-                return p_src
+            if hasattr(p, 'protocol_name') is True:
+                if p.protocol_name == 'ipv4':
+                    p_src = p.src
+                    return p_src
     return None
 
 
 def get_packet_dst_port(pkt):
     for p in pkt:
-            if p.protocol_name == 'tcp':
-                p_dstport = p.dst_port
-                return p_dstport
+            if hasattr(p, 'protocol_name') is True:
+                if p.protocol_name == 'tcp':
+                    p_dstport = p.dst_port
+                    return p_dstport
     return None
 
 
 def get_packet_src_port(pkt):
     for p in pkt:
-            if p.protocol_name == 'tcp':
-                p_srcport = p.src_port
-                return p_srcport
+            if hasattr(p, 'protocol_name') is True:
+                if p.protocol_name == 'tcp':
+                    p_srcport = p.src_port
+                    return p_srcport
     return None
 
 def get_packet_length(pkt):
     for p in pkt:
-            if p.protocol_name == 'ip':
-                p_length = p.total_length
-                return p_length
+            if hasattr(p, 'protocol_name') is True:
+                if p.protocol_name == 'ip':
+                    p_length = p.total_length
+                    return p_length
     return 0
 
 def print_packet_data(buf, total_lenth):
